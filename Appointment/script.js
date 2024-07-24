c6d859ea10c1819f0bd9ecbb5c3a223e940b5ac4
Biplove <yadavbiplove21@gmail.com>
2024-07-24 15:08:32 +0545

    Code of Appointment system

---
+++ Appointment/script.js
@@ -0,0 +1,10 @@
+function openDaySchedule(url) {
+    daySchedule.initPopupWidget({
+      url: url,
+      color: {
+        primary: '#232325',
+        secondary: '#adadad',
+      },
+    });
+  }
+  
